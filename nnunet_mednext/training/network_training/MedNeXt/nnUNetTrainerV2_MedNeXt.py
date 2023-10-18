ec87f82a7f761ad78c13dd2b38067ee3fc907245
Saikat Roy <saikat.roy@dkfz-heidelberg.de>
2023-10-18 15:19:08 +0200

    added grn trainer

--- nnunet_mednext/training/network_training/MedNeXt/nnUNetTrainerV2_MedNeXt.py
+++ nnunet_mednext/training/network_training/MedNeXt/nnUNetTrainerV2_MedNeXt.py
@@ -121,6 +121,29 @@ class nnUNetTrainerV2_MedNeXt_L_kernel3(nnUNetTrainerV2_Optim_and_LR):
             self.network.cuda()
 
 
+class nnUNetTrainerV2_MedNeXt_L_kernel3_GRN(nnUNetTrainerV2_Optim_and_LR):   
+        
+    def initialize_network(self):
+        self.network = MedNeXt(
+            in_channels = self.num_input_channels, 
+            n_channels = 32,
+            n_classes = self.num_classes, 
+            exp_r=[3,4,8,8,8,8,8,4,3],         # Expansion ratio as in Swin Transformers
+            # exp_r=[3,4,8,8,8,8,8,4,3],         # Expansion ratio as in Swin Transformers
+            kernel_size=3,                     # Can test kernel_size
+            deep_supervision=True,             # Can be used to test deep supervision
+            do_res=True,                      # Can be used to individually test residual connection
+            do_res_up_down = True,
+            # block_counts = [6,6,6,6,4,2,2,2,2],
+            block_counts = [3,4,8,8,8,8,8,4,3],
+            checkpoint_style = 'outside_block',
+            grn=True
+        )
+
+        if torch.cuda.is_available():
+            self.network.cuda()
+
+
 # Kernels of size 5
 class nnUNetTrainerV2_MedNeXt_S_kernel5(nnUNetTrainerV2_Optim_and_LR):   
 
