89a6374fe60f9dac5dd5abf380454f8c2a731342
Saikat Roy <saikat.roy@dkfz-heidelberg.de>
2023-11-08 13:31:40 +0100

    testing kernel 7

--- nnunet_mednext/training/network_training/MedNeXt/nnUNetTrainerV2_MedNeXt.py
+++ nnunet_mednext/training/network_training/MedNeXt/nnUNetTrainerV2_MedNeXt.py
@@ -388,3 +388,32 @@ class nnUNetTrainerV2_MedNeXt_L_kernel5_lr_1e_4(nnUNetTrainerV2_MedNeXt_L_kernel
         super().__init__(*args, **kwargs)
         self.initial_lr = 1e-4
 
+
+class nnUNetTrainerV2_MedNeXt_L_kernel7(nnUNetTrainerV2_Optim_and_LR):   
+    
+    def initialize_network(self):
+        self.network = MedNeXt(
+            in_channels = self.num_input_channels, 
+            n_channels = 32,
+            n_classes = self.num_classes, 
+            exp_r=[3,4,8,8,8,8,8,4,3],         # Expansion ratio as in Swin Transformers
+            kernel_size=7,                     # Can test kernel_size
+            deep_supervision=True,             # Can be used to test deep supervision
+            do_res=True,                      # Can be used to individually test residual connection
+            do_res_up_down = True,
+            # block_counts = [6,6,6,6,4,2,2,2,2],
+            block_counts = [3,4,8,8,8,8,8,4,3],
+            checkpoint_style = 'outside_block',
+            grn=True
+        )
+
+        if torch.cuda.is_available():
+            self.network.cuda()
+
+
+class nnUNetTrainerV2_MedNeXt_L_kernel7_25bpe(nnUNetTrainerV2_MedNeXt_L_kernel7):   
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # self.initial_lr = 5e-4
+        self.num_batches_per_epoch = 25
